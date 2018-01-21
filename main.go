ec43c40587a9bd59333a4bbb1b1f96cb16743dc3
Shinnosuke Sawada <sawada@nlp.ist.i.kyoto-u.ac.jp>
2018-01-21 21:38:27 +0900

    GAE/Go に移行

--- main.go
+++ main.go
@@ -1,45 +1,40 @@
-package main
+package yahbfav
 
 import (
 	"encoding/xml"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/julienschmidt/httprouter"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
-//go:generate go-assets-builder -s="/data" -o bindata.go data
+//go:generate go-assets-builder -p yahbfav -s "/data" -v Assets -o bindata.go data
 
-var tmpl = template.Must(parseFile("/template.html"))
+var tmpl = template.Must(template.ParseFiles("data/template.html"))
 
 func Favorite(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	resp, err := http.Get("http://b.hatena.ne.jp/" + ps.ByName("name") + "/favorite.rss")
+	c := urlfetch.Client(appengine.NewContext(r))
+
+	resp, err := c.Get("http://b.hatena.ne.jp/" + ps.ByName("name") + "/favorite.rss")
 	if err != nil {
-		log.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 
 	var rss RSS
 	err = xml.NewDecoder(resp.Body).Decode(&rss)
 	if err != nil {
-		log.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 
 	tmpl.Execute(w, rss.Items)
 }
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
+func init() {
 	router := httprouter.New()
-	router.Handler("GET", "/", http.FileServer(Assets))
 	router.GET("/:name", Favorite)
-
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	http.Handle("/", router)
 }
--- rss.go
+++ rss.go
@@ -1,4 +1,4 @@
-package main
+package yahbfav
 
 import (
 	"encoding/xml"
